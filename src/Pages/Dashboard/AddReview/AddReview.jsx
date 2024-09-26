fd79dbb7917c77aab7cb1053e11cb93e7abbbb44
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2024-09-27 01:28:42 +0600

    admin home component added

--- src/Pages/Dashboard/AddReview/AddReview.jsx
+++ src/Pages/Dashboard/AddReview/AddReview.jsx
@@ -1,39 +1,134 @@
-
-
-import { useState } from 'react';
-import SectionTitle from './../../Shared/SectionTitle/SectionTitle';
+import { useContext, useState } from "react";
+import SectionTitle from "./../../Shared/SectionTitle/SectionTitle";
+import { FaRocket } from "react-icons/fa6";
+import { useForm } from "react-hook-form";
+import useModal from "../../../Hooks/useModal";
+import { AuthContext } from "../../../Providers/AuthProvider";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const AddReview = () => {
-    const [rating, setRating] = useState(1);
-    console.log(rating)
-   
 
-    const handleClick = (value) => {
-        setRating(value);
-         // Call the onRate function to pass the rating to the parent component
+  const [rating, setRating] = useState(1);
+  const { openModal = {} } = useModal();
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+  console.log(rating);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const handleFormSubmit = async (data) => {
+    console.log(data);
+    const reviewData = {
+     recipe: data.recipe,
+     suggestion: data.suggestion,
+     details: data.description,
+     email: user?.email,
+     name: user?.displayName,
+     rating
     };
-    return (
-        <div>
-            <SectionTitle title='give a review' subTitle='sharing is caring!!!'></SectionTitle>
-            <div>
-                <p>RATE Us!</p>
-                <div style={{ display: 'flex', cursor: 'pointer' }} className='hover: text-yellow-500'>
-            {[1, 2, 3, 4, 5].map((value) => (
-                <svg
-                    key={value}
-                    onClick={() => handleClick(value)}
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill={value <= rating ? 'gold' : 'gray'}
-                >
-                    <path d="M12 .587l3.668 7.568 8.432 1.214-6.093 5.897 1.434 8.452L12 18.897l-7.441 3.908 1.434-8.452-6.093-5.897 8.432-1.214z" />
-                </svg>
-            ))}
+    const res = await axiosSecure.post("/review", { reviewData });
+    console.log(res.data);
+    if (res.data.insertedId) {
+      openModal({
+        title: "Review done!",
+        message: `Thanks for your valuable review`,
+        autoCloseTime: 2000,
+      });
+      reset();
+    }
+  };
+
+  const handleClick = (value) => {
+    setRating(value);
+    // Call the onRate function to pass the rating to the parent component
+  };
+  return (
+    <div>
+      <SectionTitle
+        title="give a review"
+        subTitle="sharing is caring!!!"
+      ></SectionTitle>
+      <div>
+        <p>RATE Us!</p>
+        {errors && <span>{errors.message}</span>}
+        <div
+          style={{ display: "flex", cursor: "pointer" }}
+          className="hover: text-yellow-500"
+        >
+          {[1, 2, 3, 4, 5].map((value) => (
+            <svg
+              key={value}
+              onClick={() => handleClick(value)}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill={value <= rating ? "gold" : "gray"}
+            >
+              <path d="M12 .587l3.668 7.568 8.432 1.214-6.093 5.897 1.434 8.452L12 18.897l-7.441 3.908 1.434-8.452-6.093-5.897 8.432-1.214z" />
+            </svg>
+          ))}
         </div>
+      </div>
+      {/* form section */}
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className="grid grid-cols-1 gap-6 mb-2">
+              {/* recipe section  */}
+              <div className="flex flex-col">
+                <label htmlFor="recipe" className="text-gray-300 mb-2">
+                  Which recipe you liked most?
+                </label>
+                <input
+                  {...register("recipe", { required: true })}
+                  type="text"
+                  id="recipe"
+                  className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-t-md rounded-b-none focus:outline-none focus:border-yellow-500"
+                  placeholder="Ricpe you liked most"
+                />
+              </div>
+              {/* suggestion section  */}
+              <div className="flex flex-col">
+                <label htmlFor="suggestion" className="text-gray-300 mb-2">
+                  Do you have any suggestion for us?
+                </label>
+                <input
+                  {...register("suggestion", { required: true })}
+                  type="text"
+                  id="suggestion"
+                  className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-t-md rounded-b-none focus:outline-none focus:border-yellow-500"
+                  placeholder="Suggestion"
+                />
+              </div>
+             
+              {/* description section  */}
+              <div className="flex flex-col">
+                <label htmlFor="description" className="text-gray-300 mb-2">
+                  Kindly express your care in a short way.
+                </label>
+                <input
+                  {...register("description", { required: true })}
+                  type="text"
+                  id="description"
+                  className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-t-md rounded-b-none focus:outline-none focus:border-yellow-500"
+                  placeholder="Review in detail"
+                />
+              </div>    
             </div>
-        </div>
-    );
+
+            <div className="flex justify-center mt-20">
+              <button
+                type="submit"
+                className="px-5 text-sm py-3 bg-yellow-500 text-white rounded-md hover:bg-yellow-600 transition-all duration-300"
+              >
+                <FaRocket className="inline mr-2 text-xl"></FaRocket>
+                Confirm Bookings
+              </button>
+            </div>
+          </form>
+    </div>
+  );
 };
 
 export default AddReview;
--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,6 +1,121 @@
 import { FaPhone, FaShop, FaWallet } from "react-icons/fa6";
-
+import CanvasJSReact from '@canvasjs/react-charts';
+import { useRef } from "react";
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 const AdminHome = () => {
+
+  // Original sales data in units
+	const totalSalesData = [
+		{ label: "Dessert", value: 250 },
+		{ label: "Pizza", value: 300 },
+		{ label: "Salad", value: 150 },
+		{ label: "Soup", value: 100 },
+		{ label: "Drinks", value: 200 },
+		{ label: "Others", value: 50 }
+	];
+  
+  const totalSales = totalSalesData.reduce((acc, item) => acc + item.value, 0);
+
+  const dataPoints = totalSalesData.map(item => ({y: Math.round((item.value / totalSales) * 100), label: item.label}))
+
+  console.log(dataPoints);
+
+  const option = {
+		animationEnabled: true,
+		exportEnabled: true,
+		theme: "dark1", // "light1", "dark1", "dark2"
+		title: {
+			text: ""
+		},
+		data: [{
+			type: "pie",
+			indexLabel: "{label}: {y}%",		
+			startAngle: 180,
+			dataPoints: dataPoints 
+		}]
+	};
+
+  //weekly sales chart code 
+
+  const chartRef = useRef(null);
+  // Common date points for all categories
+	const dates = [
+		new Date(2024, 5, 25),
+		new Date(2024, 5, 26),
+		new Date(2024, 5, 27),
+		new Date(2024, 5, 28),
+		new Date(2024, 5, 29),
+		new Date(2024, 5, 30),
+		new Date(2024, 6, 1)
+	];
+
+  	// Y-values for each category
+	const salesData = {
+		Salad: [56, 45, 71, 41, 60, 75, 98],
+		Pizza: [86, 95, 71, 58, 60, 65, 89],
+		Soup: [48, 45, 41, 55, 80, 85, 83],
+		Dessert: [61, 55, 61, 75, 80, 85, 105],
+		Drinks: [52, 55, 20, 35, 30, 45, 25],
+		Others: [52, 55, 20, 35, 30, 45, 25]
+	};
+
+  // Define colors for each data series
+  const colors = {
+		Salad: "red",
+		Pizza: "violet",
+		Soup: "purple",
+		Dessert: "blue",
+		Drinks: "#FFC300",
+		Others: "#FF5733",
+	};
+
+  // Construct dataPoints array for each category
+	const createDataSeries = (name, yValues) => {
+		return {
+			type: "stackedBar",
+			name: name,
+			showInLegend: true,
+      color: colors[name],
+			xValueFormatString: "DD, MMM",
+			yValueFormatString: "$#,##0",
+			dataPoints: dates.map((date, index) => ({ x: date, y: yValues[index] }))
+		};
+	};
+
+  // Construct all the data series
+	const dataSeries = Object.keys(salesData).map(category => 
+		createDataSeries(category, salesData[category])
+	);
+
+  //weekly Chart options
+	const options = {
+		animationEnabled: true,
+		theme: "dark1",
+		title: {
+			text: ""
+		},
+		axisX: {
+			valueFormatString: "DDD"
+		},
+		axisY: {
+			prefix: "$"
+		},
+		toolTip: {
+			shared: true
+		},
+		legend: {
+			cursor: "pointer",
+			itemclick: (e) => {
+				if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+					e.dataSeries.visible = false;
+				} else {
+					e.dataSeries.visible = true;
+				}
+				chartRef.current.render();
+			}
+		},
+		data: dataSeries
+	};
 
   
   return (
@@ -37,13 +152,17 @@ const AdminHome = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-2">
-        <div className=" bg-pink-300 h-auto">
+      <div className="grid grid-cols-1">
+        <div className="my-20">
           <div>
-          graph
+            <h4>Weekly sales in restaurent</h4>
+          <CanvasJSChart options={options} onRef={ref => chartRef.current = ref} />
           </div>  
         </div>
-        <div className=" bg-amber-600">activiteis</div>
+        <div>
+          <h4>Menu sale in percentage</h4>
+        <CanvasJSChart options={option} />
+        </div>
       </div>
     </div>
   );
