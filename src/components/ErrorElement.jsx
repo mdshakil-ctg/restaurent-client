ba4b80589e4f61cf51d1d2f9452f5526019f9c0c
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2024-10-10 23:19:36 +0600

    error page responsive

--- src/components/ErrorElement.jsx
+++ src/components/ErrorElement.jsx
@@ -6,16 +6,16 @@ const ErrorElement = () => {
   const navigate = useNavigate();
   const error = useRouteError();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h2 className="text-2xl font-semibold text-red-600 mb-4">
+    <div className="flex flex-col items-center justify-center text-center min-h-screen bg-white py-10 md:p-20">
+      <h2 className="text-3xl lg:text-5xl font-semibold text-red-600 mb-4">
         Oops! The page you are looking for is missing from the menu!
       </h2>
       <p><i>{error.statusText || error.message}</i></p>
-      <img src={Img} alt="Page Not Found" className="w-96 my-8" />
+      <img src={Img} alt="Page Not Found" className=" my-8" />
       <p className="text-lg text-gray-600 mb-8">
         It seems the page you ordered isnot on the menu. Lets help you find your way back!
       </p>
-      <div className="space-x-4">
+      <div className="space-x-4 space-y-4">
         <button
           onClick={() => navigate("/")}
           className="px-6 py-3 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
