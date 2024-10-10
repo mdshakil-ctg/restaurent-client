295459faafa9bb40bc81281e809810ccd838e413
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2024-10-10 15:14:38 +0600

    featured section responsive

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -5,21 +5,21 @@ import './Featured.css'
 import Button from "../../Shared/Button/Button";
 const Featured = () => {
   return (
-    <div className="mb-10 p-8 bg-fixed featured-bg flex flex-col h-screen items-center text-white">
-    <div className="z-10 mb-10 w-full">
+    <div className=" p-8 bg-fixed featured-bg flex flex-col justify-center h-screen items-center text-white">
+    <div className="z-10 w-full">
       <SectionTitle
         title="from our menu"
         subTitle="Check it out"
       ></SectionTitle>
     </div>
-    <div className="z-10 md:p-16">
-      <div className="flex items-center md:gap-4">
+    <div className="z-10 md:p-16 md:pt-0 lg:pt-16">
+      <div className="flex md:flex-col lg:flex-row items-center md:gap-4">
         <div>
           <img className="rounded-xl hidden md:flex" src={featuredImd} alt="Featured Image" />
         </div>
-        <div className=" p-12 space-y-2">
+        <div className="p-6 md:p-12 space-y-2">
           <span className="font-semibold text-xs">Septembar 20, 2024</span>
-          <h5 className="text-3xl font-satisfy">WHERE ARE YOU GET DRINKS?</h5>
+          <h5 className="text-3xl md:text-2xl font-satisfy">WHERE ARE YOU GET DRINKS?</h5>
           <p className="text-xs">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
             voluptate facere, deserunt dolores maiores quod nobis quas quasi.
